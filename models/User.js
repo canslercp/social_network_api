bc20ce3e07b1059a44d5efa0894573128c24fef6
canslercp <canslercp@gmail.com>
2022-08-15 16:57:48 -0400

    build user model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+//Schema to create User model
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/.test(v);
+                }
+            }
+        },
+        thoughts: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Thoughts'
+        }],
+        friends: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'User'
+        }]
+});
+
+// Virtual property 'commentCount' that gets the amount of comments per post
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// Initialize User model
+const User = model('user', userSchema);
+
+module.exports = User;
