798d539cefc900e757100cf93daab22d2a3ca146
canslercp <canslercp@gmail.com>
2022-08-16 18:39:21 -0400

    update controllers and route

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -51,5 +51,35 @@ module.exports = {
                 : res.json({ message: 'User successfully deleted!' })
                 )
                 .catch((err) => res.status(500).json(err));
-    }
+    },
+
+    //add a new friend to a user's friend list
+    addFriend(req, res){
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body} },
+            { runValidators: true, new: true }
+            )
+            .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with that id'})
+                : res.json(user)
+                )
+                .catch((err) => res.status(500).json(err));
+    },
+
+    //remove a friend from a user's friend list
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { reactions: { friendId: req.params.friendId }}},
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with this is'})
+                : res.json(video)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 };
